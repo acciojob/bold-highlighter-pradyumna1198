b5615d74fe399998f28f38f724909ede3baf5d9c
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-07-27 13:39:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+const  para = document.querySelector('p');
+const bTags = para.querySelectorAll('strong');
 function highlight() {
     //Write your code here
 
+
+	for(let i=0;i<btags.length;i++){
+		btags[i].style.color='green';
+	}
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
+	
 
+  // Change the color of bold words back to black
+  for (let i = 0; i < bTags.length; i++) {
+    bTags[i].style.color = 'black';
     
 }
